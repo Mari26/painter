af35ba71abd57f1459c0aeac9dc1013b1e752ac7
Mari26 <mari.qubro@gmail.com>
2020-11-19 16:46:02 +0400

    last update contact page

--- vaaaaaaaaaaaj/public/js/index.js
+++ vaaaaaaaaaaaj/public/js/index.js
@@ -1,19 +1,32 @@
 var $inputItem = $(".js-inputWrapper");
 $inputItem.length && $inputItem.each(function() {
-    var $this = $(this),
-        $input = $this.find(".formRow--input"),
-        placeholderTxt = $input.attr("placeholder"),
-        $placeholder;
 
-    $input.after('<span class="placeholder">' + placeholderTxt + "</span>"),
-        $input.attr("placeholder", ""),
-        $placeholder = $this.find(".placeholder"),
+
+    try {
+        var $this = $(this),
+            $input = $this.find(".formRow--input"),
+            placeholderTxt = $input.attr("placeholder"),
+            $placeholder;
+
+        $input.after('<span class="placeholder">' + placeholderTxt + "</span>"),
+            $input.attr("placeholder", ""),
+            $placeholder = $this.find(".placeholder"),
+
+            console.log($input);
+
+
 
         $input.val().length ? $this.addClass("active") : $this.removeClass("active"),
 
-        $input.on("focusout", function() {
-            $input.val().length ? $this.addClass("active") : $this.removeClass("active");
-        }).on("focus", function() {
-            $this.addClass("active");
-        });
+            $input.on("focusout", function() {
+                $input.val().length ? $this.addClass("active") : $this.removeClass("active");
+            }).on("focus", function() {
+                $this.addClass("active");
+            });
+    } catch (err) {
+        console.log(err);
+        console.log("trakucunebi");
+    }
+
+
 });
